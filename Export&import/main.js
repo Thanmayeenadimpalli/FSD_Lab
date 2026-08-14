a82e9e639ed6e7f803789fc9971067ded88a8c66
Thanmayee <thanmayeenadimpalli@gmail.com>
2026-08-14 09:35:18 +0530

    Ignore JavaScript files

--- Export&import/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const student_1 = require("./student");
-const teacher_1 = require("./teacher");
-let s = new student_1.Student("Thanmayee");
-let t = new teacher_1.Teacher("J. Pradeep");
-s.displayStudent();
-t.displayTeacher();
--- Genric/GenricVariables.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let numberBox = {
-    value: 80
-};
-let stringBox = {
-    value: "Hello Thanmayee"
-};
-console.log(numberBox.value);
-console.log(stringBox.value);
